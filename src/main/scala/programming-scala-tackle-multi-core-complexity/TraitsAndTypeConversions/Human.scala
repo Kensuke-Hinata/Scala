8801d8896c9b8b2f28500bb476ef90333aeb83c6
Kensuke-Hinata <cpy4ever@gmail.com>
2019-05-25 09:53:35 +0800

    add some examples of programming scala tackle multicore complexity
    Change-Id: Ie4772683fc1f4fdf46f81a92577178f78a914628

--- src/main/scala/programming-scala-tackle-multi-core-complexity/TraitsAndTypeConversions/Human.scala
+++ src/main/scala/programming-scala-tackle-multi-core-complexity/TraitsAndTypeConversions/Human.scala
@@ -1,6 +1,1 @@
-class Human(val name : String) {
-  def listen() = println("Your friend " + name + " is listening")
-}
-
-class Man(override val name : String) extends Human(name)
-class Woman(override val name : String) extends Human(name)
+class Human(val name : String) extends Friend
